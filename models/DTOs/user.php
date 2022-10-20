59a74f94b52f6145d99028ea7f8b301e5604da03
trirst <abeces968@gmail.com>
2022-10-21 00:01:48 +0700

    change user->phone_number to user->email_address

--- models/DTOs/user.php
+++ models/DTOs/user.php
@@ -6,7 +6,7 @@ class user
     var $password;
     var $first_name;
     var $last_name;
-    var $phone_number;
+    var $email_address;
     var $created_at;
     var $modified_at;
     var $deleted_at;
@@ -17,7 +17,7 @@ class user
         $_password,
         $_first_name,
         $_last_name,
-        $_phone_number,
+        $_email_address,
         $_created_at,
         $_modified_at,
         $_deleted_at
@@ -27,7 +27,7 @@ class user
         $this->password = $_password;
         $this->first_name = $_first_name;
         $this->last_name = $_last_name;
-        $this->phone_number = $_phone_number;
+        $this->email_address = $_email_address;
         $this->created_at = $_created_at;
         $this->modified_at = $_modified_at;
         $this->deleted_at = $_deleted_at;
