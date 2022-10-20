39e6e291a8eeb5feb1f657ededbf98c933436444
trirst <abeces968@gmail.com>
2022-10-21 00:50:49 +0700

    Including JS scripts as a view

--- detail.php
+++ detail.php
@@ -22,7 +22,7 @@ include_once("views/head.php");
   include_once("views/productDetail.php");
   ?>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  <?php include_once("views/scripts.php"); ?>
 </body>
 
 </html>
