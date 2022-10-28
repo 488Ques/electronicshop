ff3c1d110416197e6a08e7da7148e665e8d7caee
trirst <abeces968@gmail.com>
2022-10-28 23:58:14 +0700

    Add is_thumbnail field

--- models/DTOs/product_image.php
+++ models/DTOs/product_image.php
@@ -4,6 +4,7 @@ class product_image
     var $id;
     var $url;
     var $product_id;
+    var $is_thumbnail;
     var $created_at;
     var $modified_at;
     var $deleted_at;
@@ -12,6 +13,7 @@ class product_image
         $_id,
         $_url,
         $_product_id,
+        $_is_thumbnail,
         $_created_at,
         $_modified_at,
         $_deleted_at
@@ -19,6 +21,7 @@ class product_image
         $this->id = $_id;
         $this->url = $_url;
         $this->product_id = $_product_id;
+        $this->is_thumbnail = $_is_thumbnail;
         $this->created_at = $_created_at;
         $this->modified_at = $_modified_at;
         $this->deleted_at = $_deleted_at;
