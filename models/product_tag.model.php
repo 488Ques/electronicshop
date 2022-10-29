1fd1a89acfc5d914458c711d816d5e6e6138b2cc
trirst <abeces968@gmail.com>
2022-10-30 03:54:50 +0700

    Implement get all tags

--- models/product_tag.model.php
+++ models/product_tag.model.php
@@ -10,4 +10,27 @@ class productTagModel
     {
         $this->db = $db;
     }
+
+    public function all()
+    {
+        $stmt = 'SELECT id, name FROM product_tag';
+
+        $prepared = $this->db->prepare($stmt);
+        $prepared->execute();
+
+        $tags = array();
+        while ($temp = $prepared->fetch()) {
+            $tag = new product_tag(
+                $temp['id'],
+                $temp['name'],
+                null,
+                null,
+                null,
+                null,
+            );
+            array_push($tags, $tag);
+        };
+
+        return $tags;
+    }
 }
