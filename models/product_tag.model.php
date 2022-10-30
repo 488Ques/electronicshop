69dbeeb32db30cddb27a22f81dbd8981e6ef6d02
trirst <abeces968@gmail.com>
2022-10-30 22:41:33 +0700

    Add getProductTagsName()

--- models/product_tag.model.php
+++ models/product_tag.model.php
@@ -33,4 +33,28 @@ class productTagModel
 
         return $tags;
     }
+
+    // Get tags' ID and name of a single product
+    public function getProductTagsName($prodID)
+    {
+        $stmt = 'SELECT tag.id, tag.name FROM producttag_map map, product_tag tag WHERE product_id = ? AND map.tag_id = tag.id;';
+
+        $prepared = $this->db->prepare($stmt);
+        $prepared->execute([$prodID]);
+
+        $tags = array();
+        while ($temp = $prepared->fetch()) {
+            $tag = new product_tag(
+                $temp['id'],
+                $temp['name'],
+                null,
+                null,
+                null,
+                null,
+            );
+            array_push($tags, $tag);
+        };
+
+        return $tags;
+    }
 }
