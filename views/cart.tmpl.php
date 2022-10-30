09f926a8e272493295b9d9f871027fbe388659c6
trirst <abeces968@gmail.com>
2022-10-31 00:05:13 +0700

    Mockup cart page

---
+++ views/cart.tmpl.php
@@ -0,0 +1,44 @@
+<div class="container">
+    <div class="row mt-4">
+        <section class="col-lg-8">
+            <!-- Item-->
+            <div class="d-sm-flex justify-content-between align-items-center my-2 pb-3 border-bottom">
+                <div class="d-block d-sm-flex align-items-center text-center text-sm-start">
+                    <a class="d-inline-block flex-shrink-0 mx-auto me-sm-4" href="shop-single-v1.html">
+                        <img src="https://cdn.nguyenkimmall.com/images/detailed/776/10051129-dien-thoai-samsung-galaxy-a03-32gb-den-1.jpg" alt="Product" width="160">
+                    </a>
+
+                    <div class="pt-2">
+                        <h3 class="mb-2">
+                            <a class="text-decoration-none" href="shop-single-v1.html">Điện thoại Samsung Galaxy A03 32GB Đen</a>
+                        </h3>
+                        <div class="pt-2">2,990,000 VNĐ</div>
+                    </div>
+                </div>
+                <div class="pt-2 pt-sm-0 ps-sm-3 mx-auto mx-sm-0 text-center text-sm-start" style="max-width: 9rem;">
+                    <!-- TODO Quantity -->
+                    <!-- <label class="form-label" for="quantity1">Số lượng</label>
+                    <input class="form-control" type="number" id="quantity1" min="1" value="1"> -->
+                    <button class="btn btn-link px-0 text-danger text-decoration-none" type="button">
+                        <i class="bi bi-x-circle me-1"></i>
+                        <span>Remove</span>
+                    </button>
+                </div>
+            </div>
+        </section>
+
+        <aside class="col-lg-4">
+            <div class="bg-white rounded-3 shadow-lg p-4">
+                <div class="py-2 px-xl-2">
+                    <div class="text-center mb-4 pb-3 border-bottom">
+                        <h5 class="mb-3 pb-1 font-SamsungOne">Tổng cộng</h5>
+                        <h3 class="font-SamsungSharpSans">3,500,000 VNĐ</h3>
+                    </div>
+                    <a class="btn btn-primary btn-shadow d-block w-100 mt-4" href="#">
+                        Thanh toán
+                    </a>
+                </div>
+            </div>
+        </aside>
+    </div>
+</div>
