58e308cca4c10dc86b681952f13907b6b50c2a0a
trirst <abeces968@gmail.com>
2022-10-30 21:06:17 +0700

    Redesign nav bar

--- views/header.partial.php
+++ views/header.partial.php
@@ -5,66 +5,54 @@ session_start();
 <nav class="navbar navbar-expand-lg">
     <div class="container">
         <a class="navbar-brand" href="/">TIENTRIS</a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav mb-2 me-auto mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" href="/">Home</a>
-                <li class="nav-item dropdown">
-                    <a class="nav-link" href="#">
-                        Di động
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Điện thoại thông minh</a></li>
-                        <li><a class="dropdown-item" href="#">Máy tính bảng</a></li>
-                        <li><a class="dropdown-item" href="#">Đồng hồ thông minh</a></li>
-                    </ul>
+                    <form class="d-flex" action="/search.php">
+                        <input type="text" name="product_name" class="form-control" placeholder="Nhập tên sản phẩm" />
+                        <button class="btn btn-outline-primary ms-2">
+                            <i class="bi bi-search"></i>
+                        </button>
+                    </form>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link" href="#">
-                        TV & AV
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">TV</a></li>
-                        <li><a class="dropdown-item" href="#">Thiết bị nghe nhìn</a></li>
-                        <li><a class="dropdown-item" href="#">Máy chiếu</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown" style="margin-right: 100px;">
-                    <a class="nav-link" href="#">
-                        Gia dụng
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Tủ lạnh</a></li>
-                        <li><a class="dropdown-item" href="#">Máy giặt</a></li>
-                        <li><a class="dropdown-item" href="#">Máy hút bụi</a></li>
-                    </ul>
-                </li>
-                <form class="d-flex ">
-                    <input type="search" id="form1" class="form-control" placeholder="Nhập tên sản phẩm" />
-                    <label class="form-label" for="form1"></label>
-                    <button type="button" class="btn btn-primary btn-search">
-                        <i class="bi bi-search"></i>
-                    </button>
-                </form>
             </ul>
 
-            <ul class="navbar-nav sm-icons">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <div class="nav-link">
+                        <i class="bi bi-list-ul me-1"></i>
+                        Danh mục sản phẩm
+                    </div>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="/search.php?tag[]=1">Điện thoại</a></li>
+                        <li><a class="dropdown-item" href="/search.php?tag[]=2">Tablet</a></li>
+                        <li><a class="dropdown-item" href="/search.php?tag[]=3">Laptop</a></li>
+                        <li><a class="dropdown-item" href="/search.php?tag[]=4">Máy lạnh</a></li>
+                        <li><a class="dropdown-item" href="/search.php?tag[]=5">Tủ lạnh</a></li>
+                        <li><a class="dropdown-item" href="/search.php?tag[]=6">Máy giặt</a></li>
+                    </ul>
+                </li>
+
                 <li class="nav-item">
                     <a class="nav-link" href="#">
-                        <i class="bi bi-cart-fill" style="font-size: 25px;"></i>
+                        <i class="bi bi-cart-fill me-1" ></i>
+                        Giỏ hàng
                     </a>
                 </li>
 
                 <!-- User login/register/logout -->
                 <li class="nav-item dropdown">
                     <a class="nav-link" href="#">
-                        <i class="bi bi-person-circle me-1" style="font-size: 25px;"></i>
+                        <i class="bi bi-person-circle me-1"></i>
                         <?php
                         if (!empty($_SESSION['username'])) {
                             echo $_SESSION['username'];;
+                        } else {
+                            echo 'Tài khoản';
                         }
                         ?>
                     </a>
