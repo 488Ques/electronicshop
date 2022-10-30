b0d656399866bd13809a724b478f8b0df9625d04
trirst <abeces968@gmail.com>
2022-10-30 22:27:21 +0700

    Redesign index page's carousel

--- views/index.tmpl.php
+++ views/index.tmpl.php
@@ -1,42 +1,42 @@
 <!-- carousel -->
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+<div id="productCarousel" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button type="button" data-bs-target="#productCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#productCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#productCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="./static/image/index/slide1.jpg" class="d-block w-100" alt="...">
+            <img src="/static/image/index/1.webp" class="d-block w-100">
             <div class="carousel-caption">
-                <h5>Galaxy Z Fold4 | Flip4</h5>
+                <h5>Samsung Galaxy A03</h5>
                 <p>Ưu đãi đến <b>5 triệu</b>, hỗ trợ <b>5 triệu</b> thu cũ đổi mới và giảm thêm <b>1 triệu</b> khi thanh toán
                     trước.</p>
-                <p><a href="#" class="btn btn-primary">Mua ngay</a></p>
+                <p><a href="/detail.php?id=1" class="rounded-pill px-4 btn btn-primary">Mua ngay</a></p>
             </div>
         </div>
         <div class="carousel-item">
-            <img src="./static/image/index/galaxy-watch5-kv.png" class="d-block w-100" alt="...">
+            <img src="/static/image/index/2.webp" class="d-block w-100">
             <div class="carousel-caption">
-                <h5>Galaxy Watch5 Pro LTE</h5>
-                <p>Là smartwatch mới thay thế cho dòng Classic với diện mạo bền bỉ, chinh phục đỉnh cao.</p>
-                <p><a href="#" class="btn btn-primary">Mua ngay</a></p>
+                <h5>Tủ lạnh Panasonic Inverter</h5>
+                <p>Giữ thực phẩm tươi lâu và tiết kiệm điện gấp 2 lần.</p>
+                <p><a href="/detail.php?id=49" class="rounded-pill px-4 btn btn-primary">Mua ngay</a></p>
             </div>
         </div>
         <div class="carousel-item">
-            <img src="./static/image/index/a3194810-891e-11ec-ad3e-227a049c306f.cf_.jpg" class="d-block w-100" alt="...">
+            <img src="/static/image/index/3.webp" class="d-block w-100">
             <div class="carousel-caption">
-                <h5>Galaxy Tab S8 WIFI</h5>
+                <h5>Samsung Galaxy Tab A7</h5>
                 <p>Mang đến sự thay đổi hoàn toàn, hoàn thiện tỉ mỉ trong từng đường nét, bền bỉ vượt thời gian.</p>
-                <p><a href="#" class="btn btn-primary">Mua ngay</a></p>
+                <p><a href="/detail.php?id=17" class="rounded-pill px-4 btn btn-primary">Mua ngay</a></p>
             </div>
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#productCarousel" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#productCarousel" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
     </button>
