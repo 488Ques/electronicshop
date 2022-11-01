9cf977734d5d5c7ceb6a4240b5a70936bfce1347
trirst <abeces968@gmail.com>
2022-11-01 22:16:58 +0700

    Remove unused require

--- controllers/search.ctl.php
+++ controllers/search.ctl.php
@@ -1,14 +1,12 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . 'include_path.php');
 require_once(DIR_MODULES . 'db_module.php');
-require_once(DIR_MODELS . 'producttag_map.model.php');
 require_once(DIR_MODELS . 'product.model.php');
 require_once(DIR_MODELS . 'product_image.model.php');
 require_once(DIR_MODELS . 'product_tag.model.php');
 
 class searchController
 {
-    protected productTagMapModel $mapModel;
     protected productModel $prodModel;
     protected productImageModel $imageModel;
     protected productTagModel $tagModel;
@@ -16,7 +14,6 @@ class searchController
     public function __construct()
     {
         $db = newDB();
-        $this->mapModel = new productTagMapModel($db);
         $this->prodModel = new productModel($db);
         $this->imageModel = new productImageModel($db);
         $this->tagModel = new productTagModel($db);
