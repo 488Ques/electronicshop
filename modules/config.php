f5fab514e3c1d019c8730d1ce3a738e0127fbc05
trirst <abeces968@gmail.com>
2022-11-08 02:07:40 +0700

    Remove database account from config

--- modules/config.php
+++ modules/config.php
@@ -1,6 +1,6 @@
 <?php
 define('HOST', 'localhost');
 define('DB', 'db_electronicshop');
-define('USERNAME', 'admin');
-define('PASSWORD', '123');
+define('USERNAME', '');
+define('PASSWORD', '');
 define('CHARSET', 'utf8mb4');
